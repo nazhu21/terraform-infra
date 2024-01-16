84d0f611efad5f385776dee22a637d972b4ef839
Nazarbek <admin@Administrators-MacBook-Air.local>
2024-01-15 16:56:21 -0800

    vpc module update

--- vpc-module/outputs.tf
+++ vpc-module/outputs.tf
@@ -35,10 +35,10 @@ output "private_subnet_arns" {
 # Elastic IP Outputs (public ip and dns)
 ########################################
 
-output "eip_public_ip" {
-  value = aws_eip.elastic_ip.public_ip
-}
-output "eip_public_dns" {
-  value = aws_eip.elastic_ip.public_dns
-}
+# output "eip_public_ip" {
+#   value = aws_eip.elastic_ip.public_ip
+# }
+# output "eip_public_dns" {
+#   value = aws_eip.elastic_ip.public_dns
+# }
 
