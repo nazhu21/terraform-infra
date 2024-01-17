0b8e7b5a6253306c9c7373a8ae7e87f08180aaf4
Nazarbek <admin@Administrators-MacBook-Air.local>
2024-01-17 02:11:03 -0800

    deleteing attempt

--- project-x-roots/EKS/providers.tf
+++ project-x-roots/EKS/providers.tf
@@ -11,13 +11,17 @@ terraform {
   }
 }
 
-# data "aws_eks_cluster_auth" "cluster_auth" {
-#   name = var.cluster_name
-# }
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(aws_eks_cluster.control_plane.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-# }
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster_auth.token
+}
 
