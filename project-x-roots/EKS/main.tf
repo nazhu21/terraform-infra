9c3caf380f943b72537b60f3cb281532316b23c3
Nazarbek <admin@Administrators-MacBook-Air.local>
2024-01-17 02:35:46 -0800

    here is ur image_id

--- project-x-roots/EKS/main.tf
+++ project-x-roots/EKS/main.tf
@@ -34,6 +34,7 @@ module "eks" {
   control_plane_subnet_ids       = [module.vpc.public_subnet_ids[0], module.vpc.public_subnet_ids[1], module.vpc.public_subnet_ids[2]]
   control_plane_role_policy_arns = var.control_plane_role_policy_arns
   ebs_csi_role_policy_arns       = var.ebs_csi_role_policy_arns
+  image_id                       = var.image_id
 
   ebs_volume_size                          = var.ebs_volume_size
   volume_type                              = var.volume_type
